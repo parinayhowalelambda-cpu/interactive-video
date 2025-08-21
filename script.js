905595d07bb6d7f40d76a3df8f96f8709baadde6
parinay <parinayhowale.lambda@gmail.com>
2025-08-21 19:49:25 -0400

    commit changes

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const video = document.getElementById('myVideo');
 const poll = document.getElementById('poll');
 
-// Show poll at 10 seconds
+// Show poll at 0.23 seconds
 video.addEventListener('timeupdate', () => {
-  if (video.currentTime >= 10 && poll.style.display === "none") {
-    video.pause();
-    poll.style.display = "flex";
+  if (video.currentTime >= 0.23 && poll.style.display === "none") {
+    video.pause();              
+    poll.style.display = "flex"; 
   }
 });
 
 function submitVote(choice) {
-  // Send data to Google Sheets Web App
   fetch("https://script.google.com/macros/s/AKfycbw0_3BRMzdgbN5Z2rp6PasnDRbjaGtvMz3OolIqgch82rJKM6OZkFLuvRPrl9Gr8GaB/exec", {
     method: "POST",
     body: JSON.stringify({ answer: choice, time: new Date().toISOString() }),
     headers: { "Content-Type": "application/json" }
   }).then(() => {
     alert("Vote recorded: " + choice);
-    poll.style.display = "none";
-    video.play();
+    poll.style.display = "none"; 
+    video.play();                
   });
 }
